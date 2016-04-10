fb1e02602d61c8b98e3603cd304ec4c39fb255da
TxHawks <tofu.hawks@gmail.com>
2016-04-10 23:58:44 +0300

    Update gh-pages deployment script

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 # Generate documentation and deploy it to GitHub pages
 # http://TxHawks.github.io/jigsass-utils-size/
-gulp sass:doc
-gulp sass:sg
-git add ./docs/.
-git commit -m "Compile documentation"
-git subtree push --prefix docs origin gh-pages
+git subtree push --prefix docs/styleguide origin gh-pages
